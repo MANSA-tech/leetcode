f31b7e62a34f850289f669ae0a87117eb1d74d97
MANSA-tech <chourasiamansa@gmail.com>
2026-03-12 01:12:22 +0530

    Time: 1 ms (99.83%), Space: 43.9 MB (57.04%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public int myAtoi(String s) {
+        s=s.trim();
+        if(s.isEmpty()) return 0;
+
+        int i =0; 
+        long num=0;
+        int sign=1;
+
+        if(s.charAt(i)== '-'){
+            sign = -1;
+            i++;
+           }else if(s.charAt(i)=='+'){ 
+            sign = 1;
+            i++;
+           }
+
+            
+        while(i<s.length() && Character.isDigit(s.charAt(i))){
+            num=num*10+ s.charAt(i)-'0';
+            if(num*sign>Integer.MAX_VALUE){ 
+                return Integer.MAX_VALUE ;
+            }
+            if(num*sign<Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE ;
+                } 
+            i++; 
+        }    
+
+        return (int)num*sign;
+    }
+
+}
