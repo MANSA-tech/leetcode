01f9686aab1e77545d0fbcb5df83f55532078922
MANSA-tech <chourasiamansa@gmail.com>
2026-03-18 16:21:14 +0530

    Time: 0 ms (100%), Space: 44 MB (41.12%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // agar left half sorted hai
+            if (nums[left] <= nums[mid]) {
+                // target left half ke andar hai
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } 
+            // warna right half sorted hai
+            else {
+                // target right half ke andar hai
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
